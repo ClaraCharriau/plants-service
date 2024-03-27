acaa6329266c095cd6bb642b4ae3d74f51a8189f
Clara Charriau <clara.charriau@renault.com>
2024-03-27 16:53:53 +0100

    feat: scripts sql and docker-compose to set database

---
+++ scripts-sql/create_db.sql
@@ -0,0 +1,2 @@
+-- Active: 1711554471501@@127.0.0.1@5432@plantdex
+CREATE DATABASE plantdex;
---
+++ scripts-sql/init_db.sql
@@ -0,0 +1,10 @@
+-- Active: 1711554471501@@127.0.0.1@5432@plantdex
+DROP TABLE IF EXISTS plant;
+CREATE TABLE plant(
+  id        UUID NOT NULL PRIMARY KEY,
+  name      VARCHAR(62) NOT NULL,
+  sunlight  VARCHAR(8) NOT NULL,
+  watering  INTEGER NOT NULL,
+  category  VARCHAR(25) NOT NULL,
+  imagePath VARCHAR(93) NOT NULL
+);
