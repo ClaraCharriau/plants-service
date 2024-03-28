92449f82664923e50d5e510e09aa55a3f8f1643c
Clara Charriau <clara.charriau@renault.com>
2024-03-28 16:58:04 +0100

    chore: enable cors and change prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ dotenv.config({ path: '.env.local' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1')
   await app.listen(process.env.PORT);
 }
 bootstrap();
